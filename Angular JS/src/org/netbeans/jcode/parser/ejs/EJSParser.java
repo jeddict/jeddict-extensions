236979f75c5421bd26b9c89a0dfc903657418238
jGauravGupta <gaurav.gupta.jc@gmail.com>
2017-06-11 11:22:29 +0530

    Codebase and Library polish

--- Angular JS/src/org/netbeans/jcode/parser/ejs/EJSParser.java
+++ Angular JS/src/org/netbeans/jcode/parser/ejs/EJSParser.java
@@ -22,7 +22,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
